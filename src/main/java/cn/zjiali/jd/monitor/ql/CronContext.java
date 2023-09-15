b152de5932c9f65e648a4c9cd4481aa60cdf55ba
Gary <cnahzjl@gmail.com>
2023-09-15 15:56:06 +0800

    serial CronContext

--- src/main/java/cn/zjiali/jd/monitor/ql/CronContext.java
+++ src/main/java/cn/zjiali/jd/monitor/ql/CronContext.java
@@ -1,13 +1,14 @@
 package cn.zjiali.jd.monitor.ql;
 
+import java.io.*;
 import java.util.Set;
 
 /**
  * @author zJiaLi
  * @since 2023-03-15 09:25
  */
-public record CronContext(String cronName, String script, Set<Env> envs) {
-    public record Env(String name, String value, String remarks) {
+public record CronContext(String cronName, String script, Set<Env> envs) implements Serializable {
+    public record Env(String name, String value, String remarks) implements Serializable{
 
     }
 }
